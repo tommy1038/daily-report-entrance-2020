8b3afc24540589d7f4a0a38ac01e2e313360426a
Ryota Tomiyama <lit.karu.noir@gmail.com>
2020-05-17 20:35:16 +0900

    Updates

---
+++ precache-manifest.a90fe14e8e989b8f340183de202e07db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6aae6c41bfa68d05fb36b7d88922207",
+    "url": "/daily-report-entrance-2020/index/index.html"
+  },
+  {
+    "revision": "44efef1d57eecdea0bc1",
+    "url": "/daily-report-entrance-2020/index/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5bdaced31bef0d7f7e6",
+    "url": "/daily-report-entrance-2020/index/static/js/2.ab9520c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daily-report-entrance-2020/index/static/js/2.ab9520c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44efef1d57eecdea0bc1",
+    "url": "/daily-report-entrance-2020/index/static/js/main.a083ded9.chunk.js"
+  },
+  {
+    "revision": "6e9e7233ee04f779f888",
+    "url": "/daily-report-entrance-2020/index/static/js/runtime-main.7be5712c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daily-report-entrance-2020/index/static/media/logo.5d5d9eef.svg"
+  }
+]);
