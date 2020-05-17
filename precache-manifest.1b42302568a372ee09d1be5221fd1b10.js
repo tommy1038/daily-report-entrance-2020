9e926a9c135f6e636b169217ac8a2cd3247c2112
Ryota Tomiyama <lit.karu.noir@gmail.com>
2020-05-17 18:52:33 +0900

    Updates

---
+++ precache-manifest.1b42302568a372ee09d1be5221fd1b10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2541ae010aeb70eb6caa4b73c96d4b6c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "2731c86c0dc350e9f370",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5bdaced31bef0d7f7e6",
+    "url": "/react-app/static/js/2.ab9520c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.ab9520c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2731c86c0dc350e9f370",
+    "url": "/react-app/static/js/main.5ddccba7.chunk.js"
+  },
+  {
+    "revision": "b67b160b0964bbc216bf",
+    "url": "/react-app/static/js/runtime-main.5f9ed9b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
